7572b107d25d0e1cb83742b651b4a669e9b5e6d0
mohd zubair <mdzubair3782@gmail.com>
2022-11-26 22:35:11 +0530

    exp

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Expenses from "./Components/Expenses/Expenses";
+const App=()=> {
+  const expense = [
+    {
+      title: "Toilet Paper",
+      amount: "123.33",
+      date: new Date(2020, 3, 14),
+      location: "hyd",
+    },
+    {
+      title: "Contruct building",
+      amount: "100.33",
+      date: new Date(2021, 9, 7),
+      location: "karnatak",
+    },
+    {
+      title: "Car insurance",
+      amount: "589.33",
+      date: new Date(2022, 12, 4),
+      location: "Bidar",
+    },
+    {
+      title: "New desk",
+      amount: "149.33",
+      date: new Date(2019, 18, 6),
+      location: "Bihar",
+    },
+  ];
+  return (
+    <div>
+      <Expenses items={expense} />
+    </div>
+  );
+}
+
+export default App;
