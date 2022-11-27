bec511d9aed47416bea6c6ca0b378243b84e103e
mohd zubair <mdzubair3782@gmail.com>
2022-11-27 21:37:54 +0530

    amountbtn

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem=(props)=> {
-  const delclick=()=>{
+//function delhandler
+  const [title,setTitle]=useState(props.title)
+  const [amount,setAmount]=useState(0)
+  const clickHandler=()=>{
+    setTitle("updated")
     console.log("clicked!!!")
+  }
+  const ChangeAmount=()=>{
+  setAmount(amount+1)
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <h2>{props.location}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">${amount}</div>
       </div>
-      <button onClick={delclick()} >DELETE</button>
+      <button onClick={clickHandler} >Change the Title</button>
+      <button onClick={ChangeAmount} >AddAmount</button>
     </Card>
   );
 }
